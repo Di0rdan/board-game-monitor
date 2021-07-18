c41dace043b8f1aa16a490d1c405a00345105f17
Dmitry Vorobyev <opiopopdu@gmail.com>
2021-07-19 00:49:16 +0400

    Revoke sensitive information from repository

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import logging
 
 import bot
 
-TOKEN = "1916132812:AAGLN-5c4Io_uXQOaRyy62SobDBD9zDC0zk"
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def main():
-    telegram_bot = bot.TelegramBot(TOKEN)
+    telegram_bot = bot.TelegramBot()
     telegram_bot.register_handlers()
 
     telegram_bot.start()
